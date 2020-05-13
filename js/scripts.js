f6d375c65f67acb187a91fd4065b8bbffab0f150
fool1280 <anhnguyen@Anhs-MBP.attlocal.net>
2020-05-13 00:10:06 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+/* List of id used:
+error: what's wrong
+guess-remaining: output guess remaining
+user-guess: number user guess
+history: output history of guess
+guess: button Guess
+reset: delete array and beginning from start
+*/
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+function resetGame() {
+    number = getRandomInt(100) + 1; //random number from 1 to 100
+    guessRemaining = 5;
+    history = [];
+    historyText = "";
+    document.getElementById("error").innerHTML = null;  
+    document.getElementById("history").innerHTML = null;
+    document.getElementById("guess").disabled = false;
+    document.getElementById("guess-remaining").innerHTML = `Remaining Guesses 5`;
+    document.querySelector("div").style.backgroundColor = "whitesmoke";
+}
+function historyUpdate() {
+    l = history.length;
+    if (l === 1) {
+        historyText += history[l-1];
+    } else {
+        historyText += ", " + history[l-1];
+    }
+    document.getElementById("history").innerHTML = `${historyText}`
+}
+function guessNumber() {   
+    let guess = document.getElementById("user-guess").value;
+    guessRemaining--;
+    if (guessRemaining>0) {
+        console.log(number);
+        if (Number(guess) === number) {
+            document.getElementById("error").innerHTML = null;  
+            document.getElementById("guess-remaining").innerHTML = null;
+            document.getElementById("guess-remaining").innerHTML = `You Win!`;
+            document.getElementById("guess").disabled = true;
+            document.querySelector("div").style.backgroundColor = "#04BF8A";
+        } else {
+            let updateText = "";
+            let errorText = "";
+            if (Number(guess)<number) {
+                errorText = `Too Low with ${guess}<br>`;
+            } else {
+                errorText = `Too High with ${guess}<br>`;
+            }
+            if (guessRemaining>1) {
+                updateText += `Remaining Guesses ${guessRemaining}`;
+            } else {
+                updateText += `1 Remaining`;
+            }
+            document.getElementById("error").innerHTML = errorText;  
+            document.getElementById("guess-remaining").innerHTML = updateText;   
+            document.querySelector("div").style.backgroundColor = "#F20530"; 
+        }
+        history.push(guess);
+        historyUpdate()
+    } else {
+        document.getElementById("error").innerHTML = null;  
+        document.getElementById("guess-remaining").innerHTML = `Out of guesses! You lose :(`;
+        document.getElementById("guess").disabled = true;
+    }
+    document.getElementById("user-guess").value = null;
+}
+
+//init
+let number = getRandomInt(100) + 1; //random number from 1 to 100
+let guessRemaining = 5;
+let history = [];
+let historyText = "";
+
+
